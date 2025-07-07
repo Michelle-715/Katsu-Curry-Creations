e65d7fc8f4ea0be7e5ad12a5576b05011e3312f9
Michelle-715 <michellesagredo1@gmail.com>
2025-07-07 14:37:23 +1000

    feat: seat offer

--- katsu.py
+++ katsu.py
@@ -1,3 +1,15 @@
-print("Irasshaimase! This is Katsu Curry Creations. Please take a seat!")
+def katsu_curry_creations():
+    print("Irasshaimase! This is Katsu Curry Creations.")
+
+    while True:
+        response = input("Would you like to take a seat? (yes/no): ").strip().lower()
+        if response == "no":
+            print("No worries! Hope to see you again soon.")
+            return
+        elif response == "yes":
+            break
+        else:
+            print("Sorry, I didn't understand that.")
+
 
 print("Enjoy your katsu curry from Katsu Curry Creations! Arigatou gozaimashita!")
